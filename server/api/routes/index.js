e44c5550f2369e9fb2ebb8823b2a6b41c3f3bd5c
george-mcintyre <george@level-n.com>
2025-10-27 10:32:01 +0100

    Add `adminRoutes` to main index router; load full admin script in `admin.html`.

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/api', require('./authRoutes'));
 router.use('/api', require('./guestRoutes'));
 router.use('/api', require('./eventRoutes'));
+router.use('/', require('./adminRoutes'));
 
 module.exports = router;
